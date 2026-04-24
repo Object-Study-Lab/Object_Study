ab6b5aacdcca9e8a3d7d6c6e54396766ef0d150b
kim dong kyung <kdkd422@naver.com>
2026-04-24 23:11:07 +0900

    Chapter5: 더 개선해보자

--- week3/dk/Chapter5.kt
+++ week3/dk/Chapter5.kt
@@ -26,73 +26,15 @@ class Screening(
     }
 }
 
-abstract class Movie(
-    val discountConditions: List<DiscountCondition>,
-    val title: String,
-    val runningTime: Duration,
-    val fee: Money,
+class Movie(
+    private val title: String,
+    private val runningTime: Duration,
+    private val fee: Money,
+    private val movieType: MovieType,
+    private val discountPolicy: DiscountPolicy
 ) {
     fun calculateMovieFee(screening: Screening): Money {
-        if (isDiscountable(screening)) {
-            return fee.minus(calculatedDiscountAmount())
-        }
-        return fee
-    }
-
-    private fun isDiscountable(screening: Screening): Boolean {
-        return discountConditions.any { condition -> condition.isSatisfiedBy(screening) }
-    }
-
-    protected abstract fun calculatedDiscountAmount(): Money
-}
-
-class AmountDiscountMovie(
-    discountConditions: List<DiscountCondition>,
-    title: String,
-    runningTime: Duration,
-    fee: Money,
-    private val discountAmount: Money
-) : Movie(
-    discountConditions = discountConditions,
-    title = title,
-    runningTime = runningTime,
-    fee = fee,
-) {
-    override fun calculatedDiscountAmount(): Money {
-        return discountAmount
-    }
-}
-
-class PercentDiscountMovie(
-    discountConditions: List<DiscountCondition>,
-    title: String,
-    runningTime: Duration,
-    fee: Money,
-    private val percent: Double,
-) : Movie(
-    discountConditions = discountConditions,
-    title = title,
-    runningTime = runningTime,
-    fee = fee,
-) {
-    override fun calculatedDiscountAmount(): Money {
-        return fee.times(percent)
-    }
-}
-
-class NoneDiscountMovie(
-    discountConditions: List<DiscountCondition>,
-    title: String,
-    runningTime: Duration,
-    fee: Money,
-) : Movie(
-    discountConditions = discountConditions,
-    title = title,
-    runningTime = runningTime,
-    fee = fee,
-) {
-    override fun calculatedDiscountAmount(): Money {
-        return Money.ZERO
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening))
     }
 }
 
@@ -111,6 +53,33 @@ interface DiscountCondition {
     fun isSatisfiedBy(screening: Screening): Boolean
 }
 
+abstract class DefaultDiscountPolicy(
+    vararg conditions: DiscountCondition
+) {
+    private val conditions: List<DiscountCondition> = conditions.toList()
+
+    fun calculateDiscountAmount(screening: Screening): Money {
+        for (condition in conditions) {
+            if (condition.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening)
+            }
+        }
+        return Money.ZERO
+    }
+
+    // TEMPLATE METHOD 패턴: 부모 클래스에게 기본적인 알고리즘의 흐름을 구현하고 중간에 필요한 처리를 자식 클래스에게 위임
+    protected abstract fun getDiscountAmount(screening: Screening): Money
+}
+
+class AmountDiscountPolicy(
+    private val discountAmount: Money,
+    vararg conditions: DiscountCondition
+) : DefaultDiscountPolicy(*conditions) {
+    override fun getDiscountAmount(screening: Screening): Money {
+        return discountAmount
+    }
+}
+
 class PeriodCondition(
     private val dayOfWeek: DayOfWeek,
     private val startTime: LocalTime,
@@ -120,6 +89,16 @@ class PeriodCondition(
         return dayOfWeek == screening.whenScreened.dayOfWeek &&
                 startTime <= screening.whenScreened.toLocalTime() &&
                 endTime.isAfter(screening.whenScreened.toLocalTime())
+    }
+}
+
+interface DiscountPolicy {
+    fun calculateDiscountAmount(screening: Screening): Money
+}
+
+class NoneDiscountPolicy : DiscountPolicy {
+    override fun calculateDiscountAmount(screening: Screening): Money {
+        return Money.ZERO
     }
 }
 
