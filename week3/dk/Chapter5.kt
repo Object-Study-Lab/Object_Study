4fea4412985fbd5e8eb250d72713639fd7a7b6bc
kim dong kyung <kdkd422@naver.com>
2026-04-24 22:32:33 +0900

    Chapter5: DiscountCondition의 응집도를 어덯게 개선할 수 있을가?

---
+++ week3/dk/Chapter5.kt
@@ -0,0 +1,128 @@
+package org.example
+
+import org.example.MovieType.*
+import java.time.DayOfWeek
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+class Screening(
+    private val movie: Movie,
+    val sequence: Int,
+    val whenScreened: LocalDateTime
+) {
+
+
+    fun reserve(customer: Customer, audienceCount: Int): Reservation {
+        return Reservation(customer, this, calculateFee(audienceCount), audienceCount)
+    }
+
+    private fun calculateFee(audienceCount: Int): Money {
+        return movie.calculateMovieFee(this)
+    }
+
+    fun getSequence(): Int {
+        return sequence
+    }
+}
+
+class Movie(
+    private val title: String,
+    private val runningTime: Duration,
+    private val fee: Money,
+    private val discountConditions: List<DiscountCondition>,
+
+    private val movieType: MovieType,
+    private val discountAmount: Money,
+    private val discountPercent: Double
+) {
+    fun calculateMovieFee(screening: Screening): Money {
+
+    }
+
+    private fun isDiscountable(screening: Screening): Boolean {
+        return discountConditions.any { condition -> condition.isSatisfiedBy(screening) }
+    }
+
+    private fun calculateDiscountAmount(): Money {
+        return when (movieType) {
+            AMOUNT_DISCOUNT -> calculateAmountDiscountAmount()
+            PERCENT_DISCOUNT -> calculatePercentDiscountAmount()
+            NONE_DISCOUNT -> calculateNoneDiscountAmount()
+        }
+    }
+
+    private fun calculateAmountDiscountAmount(): Money {
+        return discountAmount
+    }
+
+    private fun calculatePercentDiscountAmount(): Money {
+        return fee.times(discountPercent)
+    }
+
+    private fun calculateNoneDiscountAmount(): Money {
+        return Money.ZERO
+    }
+}
+
+enum class MovieType {
+    AMOUNT_DISCOUNT,
+    PERCENT_DISCOUNT,
+    NONE_DISCOUNT
+}
+
+enum class DiscountConditionType {
+    SEQUENCE,
+    PERIOD
+}
+
+class DiscountCondition(
+    private val type: DiscountConditionType,
+    private val sequence: Int,
+    private val dayOfWeek: DayOfWeek,
+    private val startTime: LocalTime,
+    private val endTime: LocalTime
+) {
+
+
+    fun isSatisfiedBy(screening: Screening): Boolean {
+        if (type == DiscountConditionType.PERIOD) {
+            return isSatisfiedByPeriod(screening)
+        }
+        return isSatisfiedBySequence(screening)
+    }
+
+    private fun isSatisfiedByPeriod(screening: Screening): Boolean {
+        return dayOfWeek == screening.whenScreened.dayOfWeek &&
+                startTime <= screening.whenScreened.toLocalTime() &&
+                endTime.isAfter(screening.whenScreened.toLocalTime())
+    }
+
+    private fun isSatisfiedBySequence(screening: Screening): Boolean {
+        return sequence == screening.sequence
+    }
+}
+
+class Customer {
+
+}
+
+@JvmInline
+value class Money(
+    val amount: Double
+) {
+    operator fun times(amount: Double): Money {
+        return Money(this.amount * amount)
+    }
+
+    companion object {
+        val ZERO = Money(0)
+    }
+}
+
+class Reservation(
+    private val customer: Customer,
+    private val screening: Screening,
+    private val money: Money,
+    private val audienceCount: Int
+)
